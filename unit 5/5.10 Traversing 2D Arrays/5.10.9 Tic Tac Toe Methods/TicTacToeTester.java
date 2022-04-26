cb586c77e3a8e588c212d5362d7edbb897024456
Blazeben10 <77978015+Blazeben10@users.noreply.github.com>
2022-04-26 12:18:36 -0400

    Add files via upload

---
+++ unit 5/5.10 Traversing 2D Arrays/5.10.9 Tic Tac Toe Methods/TicTacToeTester.java
@@ -0,0 +1,27 @@
+public class TicTacToeTester extends ConsoleProgram
+{
+
+
+    public void run()
+    {
+        //This is to help you test your methods. Feel free to add code at the end to check
+        //to see if your checkWin method works!
+        TicTacToe game = new TicTacToe();
+        System.out.println("Initial Game Board:");
+        game.printBoard();
+        
+        //Prints the first row of turns taken
+        for(int row = 0; row < 3; row++)
+        {
+            if(game.pickLocation(0, row))
+            {
+                game.takeTurn(0, row);
+            }
+        }
+        System.out.println("\nAfter three turns:");
+        game.printBoard();
+        
+        
+    
+    }
+}
